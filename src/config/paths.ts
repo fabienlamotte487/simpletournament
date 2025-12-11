3ec256031f6ad7ea937d6c7344e34791e5fc2a1b
fabienlamotte487 <goodplatine@gmail.com>
2025-12-11 14:57:56 +0100

    Création du chemin pour la page history dans le fichier path

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -3,3 +3,4 @@ export const CONFIGURATION = "/configuration";
 export const PLAYGROUND = "/playground";
 export const TABLES = "/tables";
 export const CLASSEMENT = "/classement";
+export const HISTORY = "/history";
