8c66ab41c7b61f8817cdfa605ac42e60555cb391
fabienlamotte487 <goodplatine@gmail.com>
2025-12-15 11:04:37 +0100

    Après modification du store tournament, réadaptation du rendu avec les bonnes variables pour les écrans Table et Classement

--- src/components/lists/tournamentClassementPlayer/tournamentClassementPlayer.tsx
+++ src/components/lists/tournamentClassementPlayer/tournamentClassementPlayer.tsx
@@ -2,9 +2,15 @@
 
 import { useTournamentStore } from "@/src/stores/useTournamentStore";
 import ItemTournamentClassementPlayer from "./itemTournamentClassementPlayer";
+import { blank_apairying } from "@/src/hooks/preparePlayers/apairying";
 
 function TournamentClassementPlayer() {
     const {tournament} = useTournamentStore();
+    if(tournament == null || tournament.players.length === 0){
+        return null;
+    }
+
+    const matchs = blank_apairying(tournament.players);
     
     return (
         <div className='tab'>
@@ -13,7 +19,7 @@ function TournamentClassementPlayer() {
             </div>
             <div className='tab-body'>
                 <ul>
-                    {tournament?.matchs.map((p, i) => {
+                    {matchs.map((p, i) => {
                         let id = p[0].id;
                         if(p[1]){
                             id += "-" + p[1].id;
--- src/components/lists/tournamentTable/Tables.tsx
+++ src/components/lists/tournamentTable/Tables.tsx
@@ -1,11 +1,16 @@
 import { useTournamentStore } from '@/src/stores/useTournamentStore';
 import ItemTournamentTable from './ItemTournamentTable';
+import { blank_apairying } from '@/src/hooks/preparePlayers/apairying';
 
 function Tables() {
-    const {tournament} = useTournamentStore();
+  const {tournament} = useTournamentStore();
+  if(tournament == null || tournament.players.length === 0){
+      return null;
+  }
+  const matchs = blank_apairying(tournament.players);
 
-    return tournament?.matchs.map((match, i) => 
-      match[1] !== null && <ItemTournamentTable match={match} key={i} />)
+  return matchs.map((match, i) => 
+    match[1] !== null && <ItemTournamentTable match={match} key={i} />)
 }
 
 export default Tables
