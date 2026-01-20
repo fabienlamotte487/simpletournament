18d9c3834ec9b1ff7adb0fe1dae27ee12ce43f40
fabienlamotte487 <goodplatine@gmail.com>
2026-01-20 15:49:18 +0100

    Rajout de la condition du pseudo déjà pris lors de l'ajout d'un nouveau pseudo

--- src/components/forms/addNewPlayer.tsx
+++ src/components/forms/addNewPlayer.tsx
@@ -4,12 +4,14 @@ import { checkInputValue, sanitizeInput } from '../../hooks/registerPseudo/rules
 import { usePlayerStore } from '@/src/stores/usePlayerStore';
 import { registerPseudo } from '@/src/hooks/registerPseudo';
 import InputText from '@/src/ui/Form/inputText';
+import { ALREADY_TAKEN_PSEUDO } from '@/src/hooks/registerPseudo/config';
 
 export default function AddNewPlayer() {
     const [pseudo, setPseudo] = useState("");
     const [error, setError] = useState("");
     const {addNewPlayer} = usePlayerStore();
     const ref = useRef<HTMLInputElement>(null);
+    const {players} = usePlayerStore()
 
     function onSubmit(e:any){
         e.preventDefault();
@@ -17,6 +19,12 @@ export default function AddNewPlayer() {
 
         const response = registerPseudo(pseudo);
         setError(response.message);
+
+        const isPseudoAlreadyTaken = players.filter(player => player.pseudo === response.value).length >= 1;
+        if(isPseudoAlreadyTaken){
+            setError(ALREADY_TAKEN_PSEUDO);
+            response.isValid = false;
+        }
 
         if(response.isValid){
             addNewPlayer(response.value);
