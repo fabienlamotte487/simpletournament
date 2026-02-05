88f467395aae9c0fea821ea418a32c083b041e40
fabienlamotte487 <goodplatine@gmail.com>
2026-02-05 09:31:48 +0100

    Pour la page classement, triage des joueurs pour afficher correctement les joueurs par ordre de points.

--- src/components/lists/classement/Classement.tsx
+++ src/components/lists/classement/Classement.tsx
@@ -1,14 +1,19 @@
 "use client"
 import { useTournamentStore } from "@/src/stores/useTournamentStore"
+import { sortBy } from "@/src/hooks/sortBy"
 import ClassementItem from "./ClassementItem"
 
 function Classement() {
     const {tournament} = useTournamentStore()
 
+    const sortedPlayers = tournament?.players
+        ? sortBy(tournament.players, 'matchPoints', 'DESC')
+        : []
+
     return (
         <ul className="classementList">
-            {tournament?.players.map((player, index) => 
-                <ClassementItem key={index} player={player} index={index} />)}
+            {sortedPlayers.map((player, index) =>
+                <ClassementItem key={player.id} player={player} index={index} />)}
         </ul>
     )
 }
